9e6ce87ec787c3da789ce222b3b6dab29c41e0ab
Gene Panov <gene.panov@gmail.com>
2025-02-13 06:43:21 -0800

    Remove dead code

--- robotcontainer.py
+++ robotcontainer.py
@@ -35,24 +35,29 @@ class RobotContainer:
         # The driver's controller
         self.driverController = wpilib.XboxController(OIConstants.kDriverControllerPort)
 
+        # add LED strip
+        from subsystems.ledstrip import LedStrip
+        self.ledStrip1 = LedStrip(pwmChannel=1)
+
         # The robot's subsystems
         from subsystems.limelight_camera import LimelightCamera
         self.camera = LimelightCamera("limelight-aiming")  # name of your camera goes in parentheses
 
         from subsystems.intake import Intake
-        self.intake = Intake()
+        self.intake = Intake(leaderCanID=20, followerCanID=21, leaderInverted=True, followerInverted=False)
 
         self.robotDrive = DriveSubsystem()
+
         # The robots Elevator
-        from subsystems.elevator import Elevator
         from rev import LimitSwitchConfig
+        from subsystems.elevator import Elevator
         self.elevator = Elevator(leadMotorCANId=DriveConstants.kLeadElevationCanId,
                                  followMotorCANId=DriveConstants.kFollowElevationCanId,
-                                 presetSwitchPositions=(10, 20, 70), motorClass=rev.SparkMax,
+                                 presetSwitchPositions=(15, 20, 25), motorClass=rev.SparkMax,
                                  limitSwitchType=LimitSwitchConfig.Type.kNormallyClosed)
 
         self.elevator.setDefaultCommand(
-           commands2.RunCommand(lambda: self.elevator.drive(self.driverController.getRightY()), self.elevator)
+           commands2.RunCommand(lambda: self.elevator.drive(self.driverController.getRightY(), self.elevator), self.elevator)
         )
 
         leftBumper = JoystickButton(self.driverController, XboxController.Button.kLeftBumper)
@@ -62,7 +67,6 @@ class RobotContainer:
         self.localizer = Localizer(drivetrain=self.robotDrive, fieldLayoutFile="2024-crescendo.json")
         self.localizer.addPhotonCamera("front_camera", directionDegrees=0, positionFromRobotCenter=Translation2d(x=0.3, y=0.0))
         self.localizer.addPhotonCamera("left_camera", directionDegrees=+90, positionFromRobotCenter=Translation2d(x=0.3, y=0.0))
-
 
         # Configure the button bindings and autos
         self.configureButtonBindings()
@@ -97,28 +101,6 @@ class RobotContainer:
         instantiating a :GenericHID or one of its subclasses (Joystick or XboxController),
         and then passing it to a JoystickButton.
         """
-        from commands.pickup import PickupGamepiece
-        from subsystems.intake import Intake
-        pickupCommand = PickupGamepiece(self.intake, self.robotDrive, drivingSpeed=0.3)
-
-        from commands2 import InstantCommand, RunCommand
-        # RightStick = JoystickButton(self.driverController, XboxController.Button.kRightStick)
-        # RightStick.onTrue(InstantCommand(self.elevator.switchUp, self.elevator))
-
-        # assign this command to run while joystick "right bumper" button is pressed
-        rightBumper = JoystickButton(self.driverController, XboxController.Button.kRightBumper)
-        # rightBumper.whileTrue(pickupCommand)
-
-        aButton = JoystickButton(self.driverController, XboxController.Button.kA)
-        # when "A" button is pressed, start intaking the gamepiece
-        aButton.onTrue(InstantCommand(self.intake.intakeGamepiece, self.intake))
-        # when "A" button is no longer pressed, stop the intake even if it is not done
-        aButton.onFalse(InstantCommand(self.intake.stop, self.intake))
-        bButton = JoystickButton(self.driverController, XboxController.Button.kB)
-        # when "B" button is pressed, start ejecting the gamepiece
-        bButton.onTrue(InstantCommand(self.intake.ejectGamepiece, self.intake))
-        # when "B" button is no longer pressed, stop ejecting the gamepiece even if it is still in
-        bButton.onFalse(InstantCommand(self.intake.stop, self.intake))
 
         xButton = JoystickButton(self.driverController, XboxController.Button.kX)
         xButton.onTrue(ResetXY(x=0.0, y=0.0, headingDegrees=0.0, drivetrain=self.robotDrive))
@@ -141,17 +123,16 @@ class RobotContainer:
     def configureAutos(self):
         self.chosenAuto = wpilib.SendableChooser()
         # you can also set the default option, if needed
-        self.chosenAuto.setDefaultOption("Fallow coral", self.fallowcoralcommand)
+        self.chosenAuto.addOption("Fallow coral", self.fallowcoralcommand)
         self.chosenAuto.addOption("curved blue right", self.getcurvedbluerightcommand)
         self.chosenAuto.addOption("approach tag", self.getAproachTagCommand)
-        self.chosenAuto.addOption("left Blue Tag approach", self.leftBlueAprileTag)
         self.chosenAuto.addOption("go to midepoint", self.getToStage)
         wpilib.SmartDashboard.putData("Chosen Auto", self.chosenAuto)
 
     def getToStage(self):
         x = 15
         y = 2.75
-        return GoToPoint(x,y, self.robotDrive, speed=0.2)
+        return GoToPoint(x, y, self.robotDrive, speed=0.2)
 
     def getAproachTagCommand(self):
         setStartPose = ResetXY(x=0, y=0, headingDegrees=0, drivetrain=self.robotDrive)
@@ -179,10 +160,8 @@ class RobotContainer:
         swervleft = SwerveToSide(metersToTheLeft=0.2, speed=0.2, drivetrain=self.robotDrive)
         from commands.aimtodirection import AimToDirection
         aimnorth = AimToDirection(0, self.robotDrive, 0.2, 0)
-        dropcoral = None
 
         commands = setStartPose.andThen(fallowtag).andThen(alignAndPush).andThen(swervleft).andThen(aimnorth)
-        print("I created a command to approach tag")
         return commands
 
     def fallowcoralcommand(self):
@@ -196,50 +175,10 @@ class RobotContainer:
         from commands.aimtodirection import AimToDirection
 
         aimnorth = AimToDirection(0, self.robotDrive, 0.2, 0)
-        dropCoral = None
-
-        # these two lines go to __init__ function
-        from commands.pickup import PickupGamepiece
-        pickupCommand = PickupGamepiece(self.intake, self.robotDrive, drivingSpeed=0.3)
 
         commands = setStartPose.andThen(FollowCoral).andThen(alignAndPush).andThen(aimnorth)
         print("I created a command to approach coral")
         return commands
-
-
-    def leftBlueAprileTag(self):
-
-        setStartPose = ResetXY(x=0.000, y=0.000, headingDegrees=+0, drivetrain=self.robotDrive)
-
-        from commands.jerky_trajectory import JerkyTrajectory
-        trajectory = JerkyTrajectory(
-            drivetrain=self.robotDrive,
-            endpoint=(3.752, 5.422, -72.300),
-            waypoints=[
-                (0.000, 0.000, 0.000),
-                (3.600, 5,222, -72.300)
-
-            ],
-            speed=0.2
-        )
-
-        from commands.followobject import FollowObject, StopWhen
-        fallowtag = FollowObject(self.camera, self.robotDrive, stopWhen=StopWhen(maxSize=12.0), speed=0.2)
-
-        from commands.alignwithtag import AlignWithTag
-        alignAndPush = AlignWithTag(self.camera, self.robotDrive, 0, speed=0.2, pushForwardSeconds=1.1)
-
-        from commands.swervetopoint import SwerveToSide
-        swervleft = SwerveToSide(metersToTheLeft=0.2, speed=0.2, drivetrain=self.robotDrive)
-        from commands.aimtodirection import AimToDirection
-        aimnorth = AimToDirection(0, self.robotDrive, 0.2, 0)
-        dropCoral = None
-
-
-        commands = setStartPose.andthen(trajectory).andThen(fallowtag).andThen(alignAndPush).andThen(swervleft).andThen(aimnorth)
-
-
-
 
 
     def getcurvedbluerightcommand(self):
@@ -257,6 +196,8 @@ class RobotContainer:
 
         command = setStartPose.andThen(gotoFinish).andThen(aimDown)
         return command
+
+
     def getAutonomousLeftBlue(self):
         setStartPose = ResetXY(x=0.783, y=6.686, headingDegrees=+60, drivetrain=self.robotDrive)
         driveForward = commands2.RunCommand(lambda: self.robotDrive.arcadeDrive(xSpeed=1.0, rot=0.0), self.robotDrive)
@@ -272,60 +213,6 @@ class RobotContainer:
 
         command = setStartPose.andThen(driveForward.withTimeout(2.0)).andThen(stop)
         return command
-
-    def getAutonomousTrajectoryExample(self) -> commands2.Command:
-        # Create config for trajectory
-        config = TrajectoryConfig(
-            AutoConstants.kMaxSpeedMetersPerSecond,
-            AutoConstants.kMaxAccelerationMetersPerSecondSquared,
-        )
-        # Add kinematics to ensure max speed is actually obeyed
-        config.setKinematics(DriveConstants.kDriveKinematics)
-
-        # An example trajectory to follow. All units in meters.
-        exampleTrajectory = TrajectoryGenerator.generateTrajectory(
-            # Start at the origin facing the +X direction
-            Pose2d(0, 0, Rotation2d(0)),
-            # Pass through these two interior waypoints, making an 's' curve path
-            [Translation2d(0.5, 0.5), Translation2d(1, -0.5)],
-            # End 1.5 meters straight ahead of where we started, facing forward
-            Pose2d(1.5, 0, Rotation2d(0)),
-            config,
-        )
-
-        thetaController = ProfiledPIDControllerRadians(
-            AutoConstants.kPThetaController,
-            0,
-            0,
-            AutoConstants.kThetaControllerConstraints,
-        )
-        thetaController.enableContinuousInput(-math.pi, math.pi)
-
-        driveController = HolonomicDriveController(
-            PIDController(AutoConstants.kPXController, 0, 0),
-            PIDController(AutoConstants.kPXController, 0, 0),
-            thetaController,
-        )
-
-        swerveControllerCommand = commands2.SwerveControllerCommand(
-            exampleTrajectory,
-            self.robotDrive.getPose,  # Functional interface to feed supplier
-            DriveConstants.kDriveKinematics,
-            driveController,
-            self.robotDrive.setModuleStates,
-            (self.robotDrive,),
-        )
-
-        # Reset odometry to the starting pose of the trajectory.
-        self.robotDrive.resetOdometry(exampleTrajectory.initialPose())
-
-        # Run path following command, then stop at the end.
-        return swerveControllerCommand.andThen(
-            cmd.run(
-                lambda: self.robotDrive.drive(0, 0, 0, False, False),
-                self.robotDrive,
-            )
-        )
 
     def getTestCommand(self) -> typing.Optional[commands2.Command]:
         """
