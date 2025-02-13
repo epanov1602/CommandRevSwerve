6c997040a3f2f333c37228668655a8aeedd46428
Mufu <ataiwo473@gmail.com>
2025-02-13 17:00:16 -0500

    finished tuning the elevator

--- subsystems/elevator.py
+++ subsystems/elevator.py
@@ -43,9 +43,9 @@ class ElevatorConstants:
     maxArmSafeAngleDegrees = 80
 
     # PID configuration (after you are done with calibrating=True)
-    kP = 0.02  # at first make it very small like this, then start tuning by increasing from there
+    kP = 0.90  # at first make it very small like this, then start tuning by increasing from there
     kD = 0.0  # at first start from zero, and when you know your kP you can start increasing kD from some small value >0
-    kStaticGain = 0  # make it 3.5?
+    kStaticGain = 0.018  # make it 3.5?
     kMaxOutput = 1.0
 
 
