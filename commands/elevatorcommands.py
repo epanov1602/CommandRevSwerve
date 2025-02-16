afa1df164e82321e22511bdb7aa9954fab528df9
Gene Panov <gene.panov@gmail.com>
2025-02-16 10:33:33 -0800

    simplified MoveElevatorAndArm

--- commands/elevatorcommands.py
+++ commands/elevatorcommands.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 import commands2
+from commands2.waitcommand import WaitCommand
 from wpilib import Timer
 
 import constants
@@ -8,32 +9,68 @@ from subsystems.arm import Arm
 
 
 class MoveElevator(commands2.Command):
-    def __init__(self, elevator: Elevator, position: float, additionalTimeoutSeconds=0.0):
+    def __init__(self, elevator: Elevator, position: float):
         super().__init__()
         self.position = position
         self.elevator = elevator
         self.addRequirements(elevator)
-        self.additionalTimeoutSeconds = additionalTimeoutSeconds
-        self.endTime = 0.0
 
     def initialize(self):
-        self.endTime = 0.0
         self.elevator.setPositionGoal(self.position)
 
     def isFinished(self) -> bool:
-        return self.endTime != 0 and Timer.getFPGATimestamp() >= self.endTime
+        return self.elevator.isDoneMoving()
 
     def execute(self):
-        if self.endTime == 0.0 and self.elevator.isDoneMoving():
-            self.endTime = Timer.getFPGATimestamp() + self.additionalTimeoutSeconds
+        pass  # nothing to do
 
     def end(self, interrupted: bool):
         if interrupted:
             self.elevator.stopAndReset()
 
 
+class MoveArm(commands2.Command):
+    def __init__(self, arm: Arm, angle: float):
+        super().__init__()
+        self.angle = angle
+        self.arm = arm
+        self.addRequirements(arm)
+        self.endTime = 0.0
 
-class MoveElevatorAndArm(commands2.Command):
+    def initialize(self):
+        self.arm.setAngleGoal(self.angle)
+
+    def isFinished(self) -> bool:
+        return self.arm.isDoneMoving()
+
+    def execute(self):
+        pass  # nothing to do
+
+    def end(self, interrupted: bool):
+        if interrupted:
+            self.arm.stopAndReset()
+
+
+class MoveElevatorAndArm(commands2.SequentialCommandGroup):
+    def __init__(self,
+                 elevator: Elevator,
+                 position: float,
+                 arm: Arm,
+                 angle: float,
+                 safeAngle=70,
+                 additionalTimeoutSeconds=0.0):
+        # 1. make the sequential command group with three safe steps: [go to safe angle, move elevator, move arm]
+        super().__init__(
+            MoveArm(arm, safeAngle),
+            MoveElevator(elevator, position=position),
+            MoveArm(arm, angle=angle),
+            WaitCommand(seconds=additionalTimeoutSeconds)
+        )
+        # 2. assert that we have the correct requirements
+        assert set(self.requirements) == {elevator, arm}, "supposed to have {elevator, arm} requirements"
+
+
+class MoveElevatorAndArmV1(commands2.Command):
     def __init__(self, elevator: Elevator, position: float, arm: Arm, angle: float, additionalTimeoutSeconds=0.0):
         super().__init__()
         # elevator stuff
