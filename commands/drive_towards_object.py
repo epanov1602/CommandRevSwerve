f229759820ce08b7cc8b916c5fccc93ace9b2be0
lvuser <lvuser@users.noreply.github.com>
2026-01-20 11:48:16 +0100

    [drive_towards] Allow for camera to be not centered on the robot frame, when driving towards a gamepiece

--- commands/drive_towards_object.py
+++ commands/drive_towards_object.py
@@ -11,7 +11,7 @@ import typing
 import commands2
 
 from wpilib import SmartDashboard, Timer
-from wpimath.geometry import Rotation2d, Translation2d, Pose2d
+from wpimath.geometry import Rotation2d, Translation2d, Pose2d, Transform2d
 from constants import AutoConstants, DriveConstants
 from subsystems.drivesubsystem import DriveSubsystem
 
@@ -43,6 +43,7 @@ class SwerveTowardsObject(commands2.Command):
                 speed=lambda: self.driverController.getRawAxis(XboxController.Axis.kLeftTrigger),  # speed controlled by "left trigger" stick of the joystick
                 maxLateralSpeed=1.0,
                 camera=self.frontPickupCamera,
+                cameraLocationOnRobot=Pose2d(x=+0.4, y=-0.2, rotation=Rotation2d.fromDegrees(30)),  # camera located at front-right and tilted 30 degrees to the left
                 cameraPipeline=1,  # if pipeline 1 in that camera is setup to do gamepiece detection
             )
 
@@ -62,17 +63,20 @@ class SwerveTowardsObject(commands2.Command):
         drivetrain: DriveSubsystem,
         speed: typing.Callable[[], float],
         camera,
+        cameraLocationOnRobot: Pose2d = Pose2d(x=0, y=0, rotation=Rotation2d.fromDegrees(0)),
         cameraPipeline: int = -1,
         objectDiameterMeters: float = 0.2,
         maxLateralSpeed=1.0,
-        reverse=False,
     ):
         """
 
         :param drivetrain: robot drivetrain (tank or swerve)
-        :param reverse: is camera located on the reverse side of the robot?
         :param speed: speed of driving forward (not turning), probably comes from the joystick
         :param camera: camera for object detection
+        :param cameraLocationOnRobot: where is camera located on robot? ( examples:
+          frontRight=Pose2d(x=0.4, y=-0.2, rotation=Rotation2d.fromDegrees(0)),
+          rearLeft=Pose2d(x=-0.4, y=0.2, rotation=Rotation2d.fromDegrees(180)
+        )
         :param cameraPipeline: if not None, which pipeline in the camera is setup to detect this type of object
         :param objectDiameterMeters: needed for distance estimation
         :param maxLateralSpeed: maximum turning speed
@@ -89,12 +93,13 @@ class SwerveTowardsObject(commands2.Command):
         self.camera = camera
         self.addRequirements(camera)
 
+        self.cameraOnRobot = cameraLocationOnRobot
+
         assert objectDiameterMeters > 0
         self.objectDiameterMeters = objectDiameterMeters
         self.cameraPipeline = cameraPipeline
-        self.cameraStartingHeartbeat = None
+        self.camStartingHeartbeat = None
 
-        self.reverse = reverse
         self.fwdSpeed = speed
         self.maxLateralSpeed = maxLateralSpeed
 
@@ -109,64 +114,72 @@ class SwerveTowardsObject(commands2.Command):
         SmartDashboard.putString("command/c" + self.__class__.__name__, "running")
 
         # if we have a Limelight camera and it is not on the pipeline we wanted, switch it
-        self.cameraStartingHeartbeat = None
+        self.camStartingHeartbeat = None
         if hasattr(self.camera, 'getHB') and self.cameraPipeline != -1:
             if hasattr(self.camera, 'getPipeline') and hasattr(self.camera, 'setPipeline'):
                 pipelineWas = self.camera.getPipeline()
                 if pipelineWas != self.cameraPipeline:
                     self.camera.setPipeline(self.cameraPipeline)
-                    self.cameraStartingHeartbeat = self.camera.getHB() + 2
+                    self.camStartingHeartbeat = self.camera.getHB() + 2
 
 
     def execute(self):
         now = Timer.getFPGATimestamp()
-        robotPoseXY: Pose2d = self.drivetrain.getPose()
+        robotXY: Pose2d = self.drivetrain.getPose()
+        fwdSpeed = self.fwdSpeed()
 
         # 0. do we know the direction to the object? is it on the right or left of us?
-        if self.camera.hasDetection():
-            x = self.camera.getX()
-            a = self.camera.getA()
-            if (a > 0) and (self.cameraStartingHeartbeat is None) or (self.camera.getHB() >= self.cameraStartingHeartbeat):
-                distanceToTarget = self.calcualteDistanceFromDetectedObject(a)
-                SmartDashboard.putNumber("SwerveTowards/distance", distanceToTarget)
-                vectorToTarget = Translation2d(x=distanceToTarget, y=0.0).rotateBy(
-                    Rotation2d.fromDegrees(-x) + robotPoseXY.rotation()
-                )
-                if self.reverse:
-                    self.targetLocationXY = robotPoseXY.translation() - vectorToTarget
-                else:
-                    self.targetLocationXY = robotPoseXY.translation() + vectorToTarget
-                self.drivetrain.field.getObject("swerve-towards").setPoses(_square(self.targetLocationXY, side=0.1))
-        if self.lastTimeDetected is not None and now > self.lastTimeDetected + Constants.kDetectionTimeoutSeconds:
-            self.drivetrain.field.getObject("swerve-towards").setPoses([])
-            SmartDashboard.putNumber("SwerveTowards/strafe-distance", 0)
-            SmartDashboard.putNumber("SwerveTowards/distance", 0)
-            self.targetLocationXY = None
+        self.updateObjectLocation(now, robotXY)
 
-        # 1. if direction to the object unknown, stop and wait
+        # 1. if direction to the object unknown, just drive slowly
         if self.targetLocationXY is None:
-            self.drivetrain.drive(xSpeed=0, ySpeed=0, rotSpeed=0, fieldRelative=False, rateLimit=True)
+            slow = fwdSpeed / 3
+            self.drivetrain.drive(xSpeed=slow, ySpeed=0, rotSpeed=0, fieldRelative=False, rateLimit=True, square=False)
+            SmartDashboard.putNumber("SwerveTowardsObject/strafe-distance", float('nan'))
             return
 
         # 2. how far to we need to strafe to the left? (let's use the coordinates!)
-        vectorToTarget = (self.targetLocationXY - robotPoseXY.translation()).rotateBy(-robotPoseXY.rotation())
-        SmartDashboard.putNumber("SwerveTowards/strafe-distance", vectorToTarget.y)
+        vectorToTarget = (self.targetLocationXY - robotXY.translation()).rotateBy(-robotXY.rotation())
+        SmartDashboard.putNumber("SwerveTowardsObject/strafe-distance", vectorToTarget.y)
 
-        # 3. otherwise, use proportional control to drive towards it
+        # 3. use proportional control to drive towards it
         lateralSpeed = Constants.kPTranslate * abs(vectorToTarget.y)
         if Constants.kUseSqrtControl:
             lateralSpeed = math.sqrt(0.5 * lateralSpeed)
         if lateralSpeed > abs(self.maxLateralSpeed):
             lateralSpeed = abs(self.maxLateralSpeed)
-        fwdSpeed = self.fwdSpeed()
         if lateralSpeed < Constants.kMinLateralSpeed and fwdSpeed == 0:
             lateralSpeed = Constants.kMinLateralSpeed  # avoid motors stuck when turn speed is tiny and no forward motion
 
         # 4. driving time: if target is on the right, then strafe right (otherwise left)
-        if vectorToTarget.y > 0:  # y > 0 means strafe left
-            self.drivetrain.drive(fwdSpeed, +lateralSpeed, rotSpeed=0.0, fieldRelative=False, rateLimit=True)
+        if vectorToTarget.y > 0:  # y > 0 means that the object was on the left
+            self.drivetrain.drive(fwdSpeed, +lateralSpeed, 0.0, fieldRelative=False, rateLimit=True, square=False)
         else:  # y < 0 means strafe right
-            self.drivetrain.drive(fwdSpeed, -lateralSpeed, rotSpeed=0.0, fieldRelative=False, rateLimit=True)
+            self.drivetrain.drive(fwdSpeed, -lateralSpeed, 0.0, fieldRelative=False, rateLimit=True, square=False)
+
+
+    def updateObjectLocation(self, now: float | None, robotXY: Pose2d):
+        if self.camera.hasDetection():
+            x, a = self.camera.getX(), self.camera.getA()
+            if (a > 0) and (self.camStartingHeartbeat is None or self.camera.getHB() >= self.camStartingHeartbeat):
+                self.targetLocationXY = self.calculateObjectLocationXY(x, a, robotXY)
+                self.drivetrain.field.getObject("swerve-towards").setPoses(_square(self.targetLocationXY, side=0.1))
+
+        if self.lastTimeDetected is not None and now > self.lastTimeDetected + Constants.kDetectionTimeoutSeconds:
+            self.drivetrain.field.getObject("swerve-towards").setPoses([])
+            SmartDashboard.putNumber("SwerveTowardsObject/distance", 0)
+            self.targetLocationXY = None
+
+
+    def calculateObjectLocationXY(self, x, a, robotXY: Pose2d):
+        distance, direction = self.calcualteDistanceFromDetectedObject(a), Rotation2d.fromDegrees(-x)
+        SmartDashboard.putNumber("SwerveTowardsObject/distance", distance)
+
+        fromCameraToTgt = Translation2d(distance * direction.cos(), distance * direction.sin())
+        fromRobotToTgt = fromCameraToTgt.rotateBy(self.cameraOnRobot.rotation()) + self.cameraOnRobot.translation()
+        fromRobotToTgtFieldRelative = fromRobotToTgt.rotateBy(robotXY.rotation())
+
+        return robotXY.translation() + fromRobotToTgtFieldRelative
 
 
     def end(self, interrupted: bool):
