493e19eed37a97387feb2e953e20e339671980cb
714 - testing <enriquethomas79@gmail.com>
2026-01-30 18:09:23 -0500

    Robot Stays Pointed At Center When Moving

--- robotcontainer.py
+++ robotcontainer.py
@@ -45,12 +45,14 @@ class RobotContainer:
 
         self.limelightLocalizer = LimelightLocalizer(self.robotDrive)
 
-        #self.frontCamera = LimelightCamera("limelight-front")
+        self.centerCamera = LimelightCamera("limelight-center")
 
-        #self.limelightLocalizer.addCamera(
-        #    self.frontCamera,
-        #    cameraPoseOnRobot=Translation3d(x=0.40, y=-0.15, z=0.5),
-        #    cameraHeadingOnRobot=Rotation2d.fromDegrees(0.0))
+        self.limelightLocalizer.addCamera(
+            self.centerCamera,
+            cameraPoseOnRobot=Translation3d(x=0.42 , y=-0.32, z=0.5),
+            cameraHeadingOnRobot=Rotation2d.fromDegrees(0.0),
+            cameraPitchAngleDegrees=30
+        )
 
 
         # The driver's controller (joystick)
@@ -85,6 +87,22 @@ class RobotContainer:
         instantiating a :GenericHID or one of its subclasses (Joystick or XboxController),
         and then passing it to a JoystickButton.
         """
+        from commands.point_towards_location import PointTowardsLocation
+
+        # create a command for keeping the robot nose pointed towards the hub
+        keepPointingTowardsHub = PointTowardsLocation(
+            drivetrain=self.robotDrive,
+            location=Translation2d(x=4.59, y=4.025),
+            locationIfRed=Translation2d(x=11.88, y=4.025),
+        )
+
+        # set up a condition for when to do this: do it when the joystick right trigger is pressed by more than 50%
+        whenRightTriggerPressed = self.driverController.axisGreaterThan(
+            XboxController.Axis.kRightTrigger, threshold=0.5
+        )
+
+        # connect the command to its trigger
+        whenRightTriggerPressed.whileTrue(keepPointingTowardsHub)
 
         # example 1: hold the wheels in "swerve X brake" position, when "X" button is pressed
         brakeCommand = RunCommand(self.robotDrive.setX, self.robotDrive)
@@ -97,7 +115,7 @@ class RobotContainer:
         povUpButton.whileTrue(resetFacingNorthCommand)
 
         # example 3: when "POV-down" is pressed, reset robot field position to "facing South"
-        resetFacingSouthCommand = ResetXY(x=7.0, y=4.0, headingDegrees=180, drivetrain=self.robotDrive)
+        resetFacingSouthCommand = ResetXY(x=14.0, y=6.0, headingDegrees=180, drivetrain=self.robotDrive)
         povDownButton = self.driverController.povDown()
         povDownButton.whileTrue(resetFacingSouthCommand)
 
